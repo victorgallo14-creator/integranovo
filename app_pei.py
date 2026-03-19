3785dc11ef7183640aced4a8a87889ff9d14da63
victorgallo14-creator <victorgallo14@gmail.com>
2026-03-19 20:01:31 -0300

    Update app_pei.py

--- app_pei.py
+++ app_pei.py
@@ -3240,7 +3240,7 @@ elif app_mode == "👥 Gestão de Alunos":
                 # ==========================================================
                 # INÍCIO DO CONTEÚDO DO ESTUDO DE CASO (INTEGRADO NO PDI)
                 # ==========================================================
-                
+                data = data_case  # <--- ADICIONE ESTA LINHA AQUI!
                 # --- 1.1 DADOS GERAIS ---
                 pdf.add_page()
                 pdf.section_title("1.1 DADOS GERAIS DO ESTUDANTE", width=0)
